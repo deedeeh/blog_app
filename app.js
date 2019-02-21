48391c6c67fb7634478b5edded447826f42cd7e4
deedeeh <dinahafez.8@gmail.com>
2019-02-21 23:55:57 +0000

    Created show route and used substring for less text in index

--- app.js
+++ app.js
@@ -58,4 +58,15 @@ app.post('/blogs', (req, res) => {
   });
 });
 
+//SHOW ROUTE
+app.get('/blogs/:id', (req, res) => {
+  Blog.findById(req.params.id, (err, foundBlog) => {
+    if(err) {
+      res.redirect('index')
+    } else {
+      res.render('show', {blog: foundBlog});
+    }
+  })
+})
+
 app.listen(3000, console.log('Started server!'))
